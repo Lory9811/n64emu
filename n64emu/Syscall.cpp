2b3fb508309e07aba888f8331363553450b32d6f
Lorenzo Manna <lorenzo.98m@gmail.com>
2020-05-27 17:23:13 +0200

    Fixed conversion error when printing debug info.

--- n64emu/Syscall.cpp
+++ n64emu/Syscall.cpp
@@ -3,8 +3,9 @@
 
 void InterpreterCPU::Syscall(Instruction instruction, R4300* cpu) {
 	if (instruction.sa > 0) {
+		char result = instruction.rt;
 		printf("Test Result - ISA: %d Set: %d Test: %d Result: %c\n",
-			   instruction.rs, instruction.rd, instruction.sa, ((char)instruction.rt + 64));
+			   instruction.rs, instruction.rd, instruction.sa, (instruction.rt & 0x1F) + 64);
 	} else {
 
 	}
